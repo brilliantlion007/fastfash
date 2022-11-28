5c67a08ddb136dc4e40c62a3ed060689cc07708b
brilliantlion007 <119139933+brilliantlion007@users.noreply.github.com>
2022-11-28 05:22:23 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap-icons/font/bootstrap-icons.css";
 import { useState } from "react";
 import inventory from "./assets/item-data.json";
 import ClothingItem from "./components/ClothingItem";
+
 
 function App() {
 
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
   const [cartItems, setCartItems] = useState([]);
+  const [iventoryItems, setIventoryItems] = useState(inventory)
+
+
+
+  const sortCheapFirst = () => {
+    setIventoryItems((data) => {
+      const dataToSort = [...data];
+      dataToSort.sort((a, b) => Number(a.price) - Number(b.price));
+      return dataToSort;
+    });
+  };
+
+  const sortCheapLast = () => {
+    setIventoryItems((data) => {
+      const dataToSort = [...data];
+      dataToSort.sort((a, b) => (b.price -a.price ));
+      return dataToSort;
+    });
+  };
+
 
   function addToCart(item) {
     setCartItems([...cartItems, item])
@@ -21,9 +42,26 @@ function App() {
     setCartItems(cartItems.filter(item => item.name !== name))
   }
 
+  function filterInventory(param) {
+    if (param == "all") {
+      setIventoryItems(inventory)
+    } else {
+      setIventoryItems(inventory.filter(item => item.filter !== param))
+    }
+  }
+
+  // function sortByPrice(){
+  //   const sorted = iventoryItems.sort((a, b) => {
+  //     return b.price - a.price;
+  //   });
+  //   console.log(sorted)
+  //   setIventoryItems(sorted)
+  // }
+
+  
+
 
   function calculateTotal() {
-
     let total = 0
     for (let i = 0; i < cartItems.length; i++) {
       total += cartItems[i].price
@@ -43,13 +81,11 @@ function App() {
           </h1>
           <div class="d-none d-xl-block">
             <a href="" class="px-5 link-light" hreflang="ar">New in</a>
-            <a href="" class="px-5 link-light" hreflang="ar">Clothing</a>
-            <a href="" class="px-5 link-light" hreflang="ar">Shoes</a>
+            <a href="" class="px-5 link-light" hreflang="ar">Cakes</a>
             <a href="" class="px-5 link-light" hreflang="ar">Gifts</a>
             <a href="" class="px-5 link-light" hreflang="ar">Brands</a>
-            <a href="" class="px-5 link-light" hreflang="ar">Tailoring</a>
-            <a href="" class="px-5 link-light" hreflang="ar">Active Wear</a>
-            <a href="" class="px-5 link-light" hreflang="ar">Winter</a>
+            <a href="" class="px-5 link-light" hreflang="ar">Seasonal</a>
+            <a href="" class="px-5 link-light" hreflang="ar">Events </a>
             <a href="" class="px-5 link-light" hreflang="ar">Marketplace</a>
           </div>
           <div>
@@ -61,37 +97,32 @@ function App() {
         <div class="row">
           {/* main content area */}
           <div class="col-9">
-            <div class="row" style={{ paddingTop: '3%', Left: '2%' }}>
-              <div class="col">
-                <h1 class="fs-4 text-black mb-0">
-                  Current Students
+            <div class="row" style={{ paddingLeft: '2%', paddingTop: '3%' }}>
+              <div class="col-9" style={{ textAlign: 'left' }}>
+                <h1 class="fs-4 text-black mb-0 " style={{ textAlign: 'left', paddingBottom: '1%' }}>
+                  Item Categories
                 </h1>
               </div>
-              <div class="col">
-                <div class="input-group mb-3">
-                  <input type="text" class="form-control" placeholder="search for student" aria-label="Username"
-                    aria-describedby="basic-addon1"></input>
-                  <div class="input-group-append">
-                    <button type="button" class="btn btn-outline-secondary">Search</button>
-                  </div>
-                </div>
+              <div class="col-3">
+                <button type="button" class="btn btn-labeled btn-outline-dark" onClick={() => { sortCheapLast() }}> <span class="btn-label"><i class="bi bi-sort-down"></i></span> Highest First</button>
+                <button type="button" class="btn btn-labeled btn-outline-dark" onClick={() => { sortCheapFirst() }} style={{marginLeft: '5%' }}> <span class="btn-label"><i class="bi bi-sort-up"></i></span> Lowest First</button>
               </div>
             </div>
 
             {/* tabs */}
-            <div class="row" style={{ paddingLeft: '2%' }}>
+            <div class="row" style={{ paddingLeft: '2%' }}> 
+
+
+
               <ul class="nav nav-tabs">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Sculpture</a>
+                  <a class="nav-link link-dark" aria-current="page" href="#" onClick={() => { filterInventory("all") }}>All</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link px-5 link-dark" href="#">Graphic Design</a>
+                  <a class="nav-link px-5 link-dark" href="#" onClick={() => { filterInventory("Gluten") }}>Gluten Free</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link px-5 link-dark" href="#">Photography</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link px-5 link-dark" href="#">Printing</a>
+                  <a class="nav-link px-5 link-dark" href="#" onClick={() => { filterInventory("Dairy") }}>Dairy Free</a>
                 </li>
               </ul>
             </div>
@@ -100,7 +131,7 @@ function App() {
             <div class="row" style={{ paddingLeft: '2%', paddingTop: '2%' }}>
               <div class="card-group" style={{ display: 'flex', flexDirection: 'row' }}>
                 {/* map bakeryData to BakeryItem components */}
-                {inventory.map((item, index) => (
+                {iventoryItems.map((item, index) => (
                   <p><ClothingItem item={item} addToCart={addToCart} /></p>
                 ))}
               </div>
@@ -119,33 +150,15 @@ function App() {
 
               <hr></hr>
               {/* TODO: render a list of items in the cart */}
-              <ul class="nav nav-pills flex-column mb-auto" style={{textAlign: 'left'}}>
+              <ul class="nav nav-pills flex-column mb-auto" style={{ textAlign: 'left' }}>
                 {cartItems.map((item, index) => (
                   <li class="nav-item">{item.name}
-                    <button style={{float:"right"}} onClick={() => {remove4rmCart(item.name)}}>
+                    <button style={{ float: "right" }} onClick={() => { remove4rmCart(item.name) }}>
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </li>
                 ))}
-
               </ul>
-
-              {/* <ul class="list-group">
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                  Cras justo odio
-                  <span class="badge badge-primary badge-pill">14</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                  Dapibus ac facilisis in
-                  <span class="badge badge-primary badge-pill">2</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                  Morbi leo risus
-                  <span class="badge badge-primary badge-pill">1</span>
-                </li>
-              </ul> */}
-
-
               <hr></hr>
               <h1 class="fs-4 text-white mb-0" style={{
                 textAlign: 'right'
--- src/components/ClothingItem.js
+++ src/components/ClothingItem.js
@@ -1,17 +1,19 @@
 // TODO: create a component that displays a single clothing item
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function ClothingItem(props) {
     return (
-        <div class="card" style={{ width: "18rem", marginRight: "15px"}}>
+        <div class="card" style={{ width: "18rem", marginRight: "15px" }}>
+            <div class="card-header bg-transparent">{props.item.filter}</div>
             <img class="card-img-top" src={props.item.image} />
             <div class="card-body">
                 <h5 class="card-title">{props.item.name}</h5>
                 <p class="card-text">{props.item.description}</p>
             </div>
-            <div class="card-footer" style ={{textAlign: "right"}}>
+            <div class="card-footer" style={{ textAlign: "right" }}>
                 <span class="text-muted">
-                    <div style={{float:'left', fontWeight: "bold"}}>Price: ${props.item.price}</div>
-                <button class="btn btn-outline-danger"  onClick={() => {props.addToCart(props.item)}}>Add to Cart</button>
+                    <div style={{ float: 'left', fontWeight: "bold" }}>Price: ${props.item.price}</div>
+                    <button class="btn btn-outline-danger" onClick={() => { props.addToCart(props.item) }}>Add to Cart</button>
                 </span>
             </div>
 
